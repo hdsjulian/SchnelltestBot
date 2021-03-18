2a27e95c24383a16b0809f79afe6225cdc58495a
Emma Haley <emma.haley@l.de>
2021-03-18 21:48:22 +0100

    add title and description for API docs

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI
 from scraper import Scraper
-app = FastAPI()
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title='SchnelltestBot',
+    description='Returns information about availability od Covid-Antigentests in some online shops in Germany'
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='*',
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=['filename']
+)
+
 scraper = Scraper()
 scraper.all()
 
 @app.get("/")
 async def root():
+    """
+    test test test
+    :return:
+    :rtype:
+    """
     return scraper.getResults()
 
 @app.get("/dm")
