a13db89edac5576a05a0328b3052ad713103db96
JULIAN FINN <julian@julians-air.localdomain>
2021-03-18 21:56:14 +0100

    endpoint as path params

--- main.py
+++ main.py
@@ -7,16 +7,6 @@ scraper.all()
 @app.get("/")
 async def root():
     return scraper.getResults()
-
-@app.get("/dm")
-async def dm():
-    return scraper.getResults()["dm"]
-@app.get("/rossmann")
-async def rossmann():
-    return scraper.getResults()["rossmann"]
-@app.get("/lidl")
-async def lidl():
-    return scraper.getResults()["lidl"]
-@app.get("/tedi")
-async def tedi():
-    return scraper.getResults()["tedi"]
+@app.get("/markets/{marketname}")
+async def markets(marketname):
+    return scraper.getResults()[marketname]
