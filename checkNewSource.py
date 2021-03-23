42e9ae88660bb90f7d471c25b1e05af4f84fa2c9
JULIAN FINN <julian@julians-air.localdomain>
2021-03-23 23:11:05 +0100

    Added new sources and debugged some stuff

---
+++ checkNewSource.py
@@ -0,0 +1,5 @@
+import json
+from scraper import Scraper
+
+myScraper = Scraper()
+print(myScraper.statusCheck("rossmannLyher"))
--- scraper.py
+++ scraper.py
@@ -8,13 +8,16 @@ class Scraper:
     def __init__(self):
         self.http = urllib3.PoolManager()
         self.sources = {
-            'rossmann': {"name": "Rossmann", "url": "https://www.rossmann.de/de/gesundheit-boson-rapid-sars-cov-2-antigen-test/p/6921963712141", "productURL": "https://www.rossmann.de/de/gesundheit-boson-rapid-sars-cov-2-antigen-test/p/6921963712141", "function": self.rossmann, "status": False, },
+            'rossmann': {"name": "Rossmann", "url": "https://www.rossmann.de/de/gesundheit-boson-rapid-sars-cov-2-antigen-test/p/6921963712141", "productURL": "https://www.rossmann.de/de/gesundheit-boson-rapid-sars-cov-2-antigen-test/p/6921963712141", "function": self.rossmann, "status": False},
             'dm': {"name": "dm", "url": "https://products.dm.de/product/de/products/gtins/6921963712141?view=details", "productURL": "https://www.dm.de/boson-corona-schnelltest-selbsttest-p6921963712141.html", "function": self.dm, "status": False},
             'lidl': {"name": "Lidl", "url":"https://www.lidl.de/de/5er-set-corona-sars-cov-2-antigenschnelltest-boson/p374797", "productURL": "https://www.lidl.de/de/5er-set-corona-sars-cov-2-antigenschnelltest-boson/p374797","function": self.lidl,  "status": False},
             'tedi': {"name": "Tedi", "url":"https://tedi.de/LYHER-Covid-19-Antigen-Schnelltest-Nasal-Einzelverpackung", "productURL":"https://tedi.de/LYHER-Covid-19-Antigen-Schnelltest-Nasal-Einzelverpackung'", "function": self.tedi, "status": False},
             'mueller' : {"name": "Mueller", "url":"https://www.mueller.de/p/hotgen-covid-19-antigen-nasal-schnelltest-2718500/", "productURL":"https://www.mueller.de/p/hotgen-covid-19-antigen-nasal-schnelltest-2718500/", "function": self.mueller, "status": False},
-            'mueller2' : {"name": "Mueller", "url":"https://www.mueller.de/p/hotgen-covid-19-antigen-nasal-selbsttest-2721685/", "productURL":"https://www.mueller.de/p/hotgen-covid-19-antigen-nasal-selbsttest-2721685/", "function": self.mueller, "status": False}
-            #'doccheck': {
+            'mueller2' : {"name": "Mueller", "url":"https://www.mueller.de/p/hotgen-covid-19-antigen-nasal-selbsttest-2721685/", "productURL":"https://www.mueller.de/p/hotgen-covid-19-antigen-nasal-selbsttest-2721685/", "function": self.mueller, "status": False},
+            'rossmannHotgen': {"name": "Rossmann", "url": "https://www.rossmann.de/de/gesundheit-hotgen-coronavirus2019-ncov-antigentest/p/6970297534073", "productURL": "https://www.rossmann.de/de/gesundheit-hotgen-coronavirus2019-ncov-antigentest/p/6970297534073", "function": self.rossmann, "status": False}, 
+            'rossmannLyher': {"name": "Rossmann", "url": "https://www.rossmann.de/de/gesundheit-lyher-antigen-testkit/p/6972412610280", "productURL": "https://www.rossmann.de/de/gesundheit-lyher-antigen-testkit/p/6972412610280", "function": self.rossmann, "status": False}, 
+            'dmLyher': {"name": "dm", "url": "https://products.dm.de/product/de/products/gtins/6972412610280?view=details", "productURL": "https://www.dm.de/lyher-corona-schnelltest-selbsttest-p6972412610280.html", "function": self.dm, "status": False  }
+                        #'doccheck': {
             #    'name': 'DocCheck',
             #    'url': 'https://www.doccheckshop.eu/laboratory/tests/rapid-coronavirus-tests/12076/roche-sars-cov-2-rapid-antigen-test',
             #    'productURL': 'https://www.doccheckshop.eu/laboratory/tests/rapid-coronavirus-tests/12076/roche-sars-cov-2-rapid-antigen-test',
@@ -30,10 +33,16 @@ class Scraper:
             self.sources[k]["changed"] = False
         
     def statusCheck(self, market):
-        newStatus = self.sources[market]["function"]()
+        newStatus = self.sources[market]["function"](market)
         if (self.sources[market]["status"] != newStatus): 
             self.sources[market]["status"] = newStatus
             self.sources[market]["changed"] = True
+            f = open("stati.txt", "w")
+            stati = {}
+            for k, v in self.sources.items():
+                stati[k] = v["status"]
+            f.write(json.dumps(stati))
+            f.close()
             return True
         else: 
             self.sources[market]["changed"] = False
@@ -50,42 +59,44 @@ class Scraper:
         else:
             return True
 
-    def mueller(self, counter= 0):
-        infile = self.http.request('GET', self.sources['mueller']["url"], redirect=False, headers={
+    def mueller(self, market="mueller", counter= 0):
+        infile = self.http.request('GET', self.sources[market]["url"], redirect=False, headers={
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
         })
         if (infile.status !=200):
             if (counter <3):
                 counter += 1
                 time.sleep(10)
-                if self.mueller(counter) == True:
+                if self.mueller(market, counter) == True:
                     return True
             return False
         else:
             return True            
 
-    def rossmann(self):
-        infile = self.http.request('GET', self.sources["rossmann"]["url"], redirect=False, headers={
+    def rossmann(self, market = "rossmann"):
+        infile = self.http.request('GET', self.sources[market]["url"], redirect=False, headers={
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
         })
         if (infile.status == 200): 
+            match = re.findall("(Aktuell nicht)", infile.data.decode('utf-8'))
+            if (len(match) > 0): 
+                return False
             return True
         if (infile.status == 301):
             return False
 
-    def dm(self): 
-        infile = self.http.request('GET', self.sources["dm"]["url"], redirect=False, headers={
+    def dm(self, market="dm"): 
+        infile = self.http.request('GET', self.sources[market]["url"], redirect=False, headers={
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
         })
         foo = json.loads(infile.data.decode('utf-8'))
-
         if (foo[0]["purchasable"] == True):
             return True
         if (foo[0]["purchasable"] == False):
             return False
 
-    def lidl(self):
-        infile = self.http.request('GET', self.sources["lidl"]["url"], redirect=False, headers={
+    def lidl(self, market="lidl"):
+        infile = self.http.request('GET', self.sources[market]["url"], redirect=False, headers={
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
         })
         if (infile.status != 200):
@@ -96,8 +107,8 @@ class Scraper:
         if match[0] == "true":
             return True
 
-    def tedi(self):
-        infile = self.http.request('GET', self.sources["tedi"]["url"], redirect=False, headers={
+    def tedi(self, market="tedi"):
+        infile = self.http.request('GET', self.sources[market]["url"], redirect=False, headers={
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
         })
         if (infile.status != 200):
