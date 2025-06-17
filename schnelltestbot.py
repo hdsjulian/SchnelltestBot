a3394a522da62b1ef5c7a41c11862465c0a30da4
Julian Finn <julian@phinn.de>
2025-06-17 17:00:29 +0200

    output

--- schnelltestbot.py
+++ schnelltestbot.py
@@ -10,6 +10,8 @@ parser = argparse.ArgumentParser(description='A bot that sends a message every 6
 parser.add_argument('min_sleep', type=int, help='Minimum sleep time in seconds', default=900)
 parser.add_argument('max_sleep', type=int, help='Maximum sleep time in seconds', default=3600)
 args = parser.parse_args()
+print(f"Minimum sleep time: {args.min_sleep} seconds")
+print(f"Maximum sleep time: {args.max_sleep} seconds")  
 #debug id
 chatId = -1002868327646
 #real id
@@ -17,6 +19,7 @@ chatId = -1002868327646
 bot = Bot(botcode)
 
 while True: 
+
     number = random.randint(args.min_sleep, args.max_sleep)
     print(f"Sleeping for {number} seconds")
     sleep(number)
